e3032e3150cdd27f47dbe8af2ce905e34365a81c
Aditya030102 <adityavardhan950@gmail.com>
2025-04-12 14:43:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const comics = [
   { title: "Spider-Man: Homecoming", hero: "spiderman", image: "https://m.media-amazon.com/images/I/61Rltz6BB+L._AC_UF894,1000_QL80_.jpg" },
   { title: "Iron Man: Extremis", hero: "ironman", image: "https://m.media-amazon.com/images/I/615Dgl+QelL._UF1000,1000_QL80_.jpg" },
   { title: "Thor: God of Thunder", hero: "thor", image: "https://m.media-amazon.com/images/I/813hHW0KmcL.jpg" },
-  { title: "Captain America: Winter Soldier", hero: "captain",image: "https://m.media-amazon.com/images/I/71YEQOc-UnL._AC_UF1000,1000_QL80_.jpg" },
+  { title: "Captain America: Winter Soldier", hero: "captain", image: "https://m.media-amazon.com/images/I/71YEQOc-UnL._AC_UF1000,1000_QL80_.jpg" },
   { title: "Spider-Man: Into the Spider-Verse", hero: "spiderman", image: "https://m.media-amazon.com/images/I/81fdOe6LbdL._AC_UF1000,1000_QL80_.jpg" },
   { title: "Iron Man: The Invincible", hero: "ironman", image: "https://i.ebayimg.com/00/s/MTQ0OFg5Mzg=/z/50AAAOSwNIVmaedQ/$_57.JPG?set_id=880000500F" },
 ];
